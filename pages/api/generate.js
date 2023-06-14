e74a8c53ced04eb088b841e396ee9d2323e4e70e
Greg Gorlen <gsgorlen@gmail.com>
2023-06-13 19:44:13 -0700

    Implement streaming

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,4 +1,4 @@
-import { Configuration, OpenAIApi } from "openai";
+import {Configuration, OpenAIApi} from "openai";
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -7,56 +7,124 @@ const openai = new OpenAIApi(configuration);
 
 export default async function (req, res) {
   if (!configuration.apiKey) {
-    res.status(500).json({
+    // TODO convert to stream
+    return res.status(500).json({
       error: {
-        message: "OpenAI API key not configured, please follow instructions in README.md",
-      }
+        message:
+          "OpenAI API key not configured, please follow instructions in README.md",
+      },
     });
-    return;
   }
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
-    res.status(400).json({
+  const input = req.body.input || "";
+
+  if (input.trim().length === 0) {
+    return res.status(400).json({
       error: {
-        message: "Please enter a valid animal",
-      }
+        message: "Please enter a valid input",
+      },
     });
-    return;
   }
 
   try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: generatePrompt(animal),
-      temperature: 0.6,
+    const completion = await openai.createCompletion(
+      {
+        model: "text-davinci-003",
+        prompt: input,
+        max_tokens: 1000,
+        temperature: 0.9,
+        stream: true,
+      },
+      {
+        responseType: "stream",
+      }
+    );
+
+    res.writeHead(200, {
+      "Connection": "keep-alive",
+      "Content-Encoding": "none",
+      "Cache-Control": "no-cache",
+      "Content-Type": "text/event-stream",
     });
-    res.status(200).json({ result: completion.data.choices[0].text });
-  } catch(error) {
-    // Consider adjusting the error handling logic for your use case
-    if (error.response) {
-      console.error(error.response.status, error.response.data);
-      res.status(error.response.status).json(error.response.data);
-    } else {
-      console.error(`Error with OpenAI API request: ${error.message}`);
-      res.status(500).json({
-        error: {
-          message: 'An error occurred during your request.',
+
+    let finalMessage = "";
+
+    completion.data.on("end", data => {
+      console.log(finalMessage);
+      res.end();
+    });
+    completion.data.on("data", data => {
+      const chunks = data
+        .toString()
+        .split(/\n\n/)
+        .filter(Boolean);
+
+      for (const chunk of chunks) {
+        const message = chunk.replace(/^data: /, "");
+
+        if (message === "[DONE]") {
+          return;
+        }
+
+        try {
+          const parsed = JSON.parse(message);
+          const text = parsed.choices[0].text;
+          finalMessage += text;
+          res.write(
+            `data: ${JSON.stringify({chunk: text})}\n\n`
+          );
+        } catch (error) {
+          console.error(
+            "Could not JSON parse stream message",
+            message,
+            error
+          );
+        }
+      }
+    });
+  } catch (error) {
+    if (error.response?.status) {
+      console.error(error.response.status, error.message);
+      error.response.data.on("data", data => {
+        const message = data.toString();
+
+        try {
+          const parsed = JSON.parse(message);
+          console.error(
+            "An error occurred during OpenAI request: ",
+            parsed
+          );
+        } catch (error) {
+          console.error(
+            "An error occurred during OpenAI request: ",
+            message
+          );
         }
       });
+    } else {
+      console.error(
+        "An error occurred during OpenAI request",
+        error
+      );
     }
+
+    //res.end();
   }
 }
-
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+/*
+} catch(error) {
+  // Consider adjusting the error handling logic for your use case
+  if (error.response) {
+    console.error(error.response.status, error.response.data);
+    res.status(error.response.status).json(error.response.data);
+  } else {
+    console.error(`Error with OpenAI API request: ${error.message}`);
+    res.status(500).json({
+      error: {
+        message: "An error occurred during your request.",
+      }
+    });
+  }
 }
+*/
+
--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,87 @@
+import {marked} from "marked";
 import Head from "next/head";
-import { useState } from "react";
+import {useState} from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
-  const [result, setResult] = useState();
+  const [input, setInput] = useState("");
+  const [response, setResponse] = useState("");
+  const [requestActive, setRequestActive] = useState(false);
 
-  async function onSubmit(event) {
+  const onSubmit = async event => {
     event.preventDefault();
+
     try {
+      setRequestActive(true);
       const response = await fetch("/api/generate", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
+          "Accept": "text/event-stream",
         },
-        body: JSON.stringify({ animal: animalInput }),
+        body: JSON.stringify({input}),
       });
 
-      const data = await response.json();
-      if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
+      if (!response.ok) {
+        console.error(response.statusText);
+        throw Error(response.statusText);
       }
 
-      setResult(data.result);
-      setAnimalInput("");
-    } catch(error) {
-      // Consider implementing your own error handling logic here
+      setResponse("");
+
+      for (const reader = response.body.getReader(); ; ) {
+        const {value, done} = await reader.read();
+
+        if (done) {
+          break;
+        }
+
+        const chunk = new TextDecoder().decode(value);
+        const subChunks = chunk.split(/(?<=})\n\ndata: (?={)/);
+
+        for (const subChunk of subChunks) {
+          const payload = subChunk.replace(/^data: /, "");
+          setResponse(prev => prev + JSON.parse(payload).chunk);
+        }
+      }
+    } catch (error) {
       console.error(error);
-      alert(error.message);
+    } finally {
+      setRequestActive(false);
     }
-  }
+  };
 
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
+        <title>OpenAI Streaming</title>
       </Head>
-
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <h1>OpenAI Streaming</h1>
         <form onSubmit={onSubmit}>
+          <div>
+            <textarea
+              name="input"
+              placeholder="Ask a question"
+              value={input}
+              onChange={e => setInput(e.target.value)}
+            ></textarea>
+          </div>
           <input
-            type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+            type="submit"
+            value="Submit"
+            disabled={requestActive}
           />
-          <input type="submit" value="Generate names" />
         </form>
-        <div className={styles.result}>{result}</div>
+        <div
+          className={styles.response}
+          dangerouslySetInnerHTML={{
+            __html: marked.parse(response, {
+              mangle: false,
+              headerIds: false,
+            }),
+          }}
+        ></div>
       </main>
     </div>
   );
